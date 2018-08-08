492746cf5f404acaea93327ad466989325523091
Unknown <jhobeck376@west-mec.org>
2018-08-08 13:02:49 -0700

    Snoot update 2
    Made more progress towards bulletproofing the date selectors.

--- snoot.js
+++ snoot.js
@@ -6,19 +6,55 @@
     Date:   8.6.18
 */
 
-'use strict';
+"use strict";
+
+//global variables
+var twentyNine = document.createDocumentFragment();
+var thirty = document.createDocumentFragment();
+var thirtyOne = document.createDocumentFragment();
 
 // function to remove select defaults
-
-function removeSelectDefaults(){
+function removeSelectDefaults() {
     var emptyBoxes = document.getElementsByTagName("select");
-    alert("select lists: " + emptyBoxes);
+    for (var i = 0; i < emptyBoxes.length; i++){
+        emptyBoxes[i].selectedIndex = -1;
+    }
+}
+
+// function to set up document fragments for days of the month
+function setUpDays(){
+    var dates = document.getElementById("delivDy").getElementsByTagName("option");
+    twentyNine.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[28].cloneNode(true));
+    thirtyOne.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[30].cloneNode(true));
+}
+
+// set up the list of days based on the selected month
+function updateDays() {
+   var deliveryDay = document.getElementById("delivDy");
+   var dates = deliveryDay.getElementsByTagName("option");
+   var deliveryMonth = document.getElementById("delivMo");
+   var deliveryYear = document.getElementById("delivYr");
+   var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
+   while (dates[28]) {
+       deliveryDay.removeChild(dates[28]);
+   }
+   if(deliveryYear.selectedIndex === -1) {
+       deliveryDay.selectedIndex = 0;
+   }
+}
+
+// function that sets up page on a load event
+function setUpPage(){
+    removeSelectDefaults();
 }
 
 // page load event handlers
-
 if(window.addEventListener) {
-    window.addEventListener("load", removeSelectDefaults, false);
+    window.addEventListener("load", setUpPage, false);
 }else if (window.attachEvent){
-    window.attachEvent("onload", removeSelectDefaults);
+    window.attachEvent("onload", setUpPage);
 }

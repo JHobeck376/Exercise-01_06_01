e55b4a1054fc72cbc513dda292ac91816473af9f
Unknown <jhobeck376@west-mec.org>
2018-08-09 13:04:31 -0700

    Snoot update 3
    Finished up the date wheel

--- snoot.js
+++ snoot.js
@@ -16,13 +16,13 @@ var thirtyOne = document.createDocumentFragment();
 // function to remove select defaults
 function removeSelectDefaults() {
     var emptyBoxes = document.getElementsByTagName("select");
-    for (var i = 0; i < emptyBoxes.length; i++){
+    for (var i = 0; i < emptyBoxes.length; i++) {
         emptyBoxes[i].selectedIndex = -1;
     }
 }
 
 // function to set up document fragments for days of the month
-function setUpDays(){
+function setUpDays() {
     var dates = document.getElementById("delivDy").getElementsByTagName("option");
     twentyNine.appendChild(dates[28].cloneNode(true));
     thirty.appendChild(dates[28].cloneNode(true));
@@ -34,27 +34,57 @@ function setUpDays(){
 
 // set up the list of days based on the selected month
 function updateDays() {
-   var deliveryDay = document.getElementById("delivDy");
-   var dates = deliveryDay.getElementsByTagName("option");
-   var deliveryMonth = document.getElementById("delivMo");
-   var deliveryYear = document.getElementById("delivYr");
-   var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
-   while (dates[28]) {
-       deliveryDay.removeChild(dates[28]);
-   }
-   if(deliveryYear.selectedIndex === -1) {
-       deliveryDay.selectedIndex = 0;
-   }
+    var deliveryDay = document.getElementById("delivDy");
+    var dates = deliveryDay.getElementsByTagName("option");
+    var deliveryMonth = document.getElementById("delivMo");
+    var deliveryYear = document.getElementById("delivYr");
+    var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
+    while (dates[28]) {
+        deliveryDay.removeChild(dates[28]);
+    }
+    if (deliveryYear.selectedIndex === -1) {
+        deliveryDay.selectedIndex = 0;
+    }
+    // if feb and 2020 twentyNine
+    if(selectedMonth === "2" && deliveryYear.options[deliveryYear.selectedIndex].value === "2020") {
+        deliveryDay.appendChild(twentyNine.cloneNode(true));
+    }
+    // else if 30 day month thirty
+    else if(selectedMonth === "4" || selectedMonth === "6" || selectedMonth === "9" || selectedMonth === "11") {
+        deliveryDay.appendChild(thirty.cloneNode(true));
+    }
+    // else if 31 day month thirtyOne
+    else if(selectedMonth === "1" || selectedMonth === "3" || selectedMonth === "5" || selectedMonth === "7" || selectedMonth === "8" || selectedMonth === "10" || selectedMonth === "12") {
+        deliveryDay.appendChild(thirtyOne.cloneNode(true));
+    }
 }
 
 // function that sets up page on a load event
-function setUpPage(){
+function setUpPage() {
     removeSelectDefaults();
+    setUpDays();
+    createEventListeners();
+}
+
+// build a function to create our event listeners
+function createEventListeners() {
+    var deliveryMonth = document.getElementById("delivMo");
+    if (deliveryMonth.addEventListener) {
+    deliveryMonth.addEventListener("change", updateDays, false);
+    }else if (deliveryMonth.attachEvent) {
+    deliveryMonth.attachEvent("onchange", updateDays);
+    }
+    var deliveryYear = document.getElementById("delivYr");
+    if (deliveryYear.addEventListener) {
+    deliveryYear.addEventListener("change", updateDays, false);
+    }else if (deliveryYear.attachEvent) {
+    deliveryYear.attachEvent("onchange", updateDays);
+    }
 }
 
 // page load event handlers
-if(window.addEventListener) {
+if (window.addEventListener) {
     window.addEventListener("load", setUpPage, false);
-}else if (window.attachEvent){
+} else if (window.attachEvent) {
     window.attachEvent("onload", setUpPage);
 }
